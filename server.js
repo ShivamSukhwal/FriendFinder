6830f0c63c747684d9fa3ea6d9a773e11e27bf27
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2019-08-10 11:36:16 -0400

    server complete

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/api-routes")(app);
+require("./app/routing/html-routes")(app);
+
 app.listen(port, function() {
   console.log("FriendFinder listening on PORT " + port);
 });
