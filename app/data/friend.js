53f20706d4d9f727a163d06cd401a19efd592439
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2019-08-10 11:48:15 -0400

    friend database complete

---
+++ app/data/friend.js
@@ -0,0 +1,42 @@
+var friends = [
+  {
+    name: "Shivam",
+    photo: "",
+    score: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  },
+  {
+    name: "Joe",
+    photo: "",
+    score: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Eric",
+    photo: "",
+    score: [2, 2, 3, 3, 2, 2, 2, 3, 4, 4]
+  },
+  {
+    name: "Cartman",
+    photo: "",
+    score: [1, 2, 3, 4, 5, 6, 7, 8, 9, 2]
+  },
+  {
+    name: "Jacob",
+    photo: "",
+    score: [3, 3, 2, 1, 5, 1, 1, 2, 3, 4]
+  },
+  {
+    name: "Shelly",
+    photo: "",
+    score: [4, 1, 3, 5, 2, 2, 1, 4, 4, 5]
+  },
+  {
+    name: "Donna",
+    photo: "",
+    score: [1, 2, 3, 2, 3, 1, 2, 2, 4, 2]
+  },
+  {
+    name: "Kelly",
+    photo: "",
+    score: [3, 3, 3, 4, 4, 5, 5, 1, 2, 3]
+  }
+];
