ad482804b3d193f90922198298b6b9503be9c40d
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2019-08-13 13:56:02 -0400

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
